1855c982b4ef6661b6d027bb0755e4ca5b564ea6
whwnddnwhwnd <65517663+whwnddnwhwnd@users.noreply.github.com>
2021-07-09 14:17:33 +0900

    Update change.java

--- change.java
+++ change.java
@@ -4,7 +4,7 @@ class change{
      int b = 100;
      
      System.out.println("바꾸기 전 a의 값: " + a);
-	   System.out.println("바꾸기 전 b의 값: " + b);
+     System.out.println("바꾸기 전 b의 값: " + b);
      
      int temp; //temp라는 정수값을 담을 수 있는 변수 선언
      temp = a; //temp에 a의 값인 10을 저장     
@@ -12,6 +12,6 @@ class change{
      b = temp; // b에 temp의 값인 10을 저장 (현재 a = 100, b = 10, temp = 10)
      
      System.out.println("바꾼 후 a의 값: " + a);
-	   System.out.println("바꾼 후 b의 값: " + b);
+     System.out.println("바꾼 후 b의 값: " + b);
   }
 }
